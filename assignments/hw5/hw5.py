e53f3f685fd36f88cffbb0d6b0406ffb45569c66
jdelguercio <delguerciojj@g.cofc.edu>
2022-02-14 14:25:12 -0500

    lab4 done

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -1,30 +1,52 @@
 """
-Name: <your name goes here – first and last>
+Name: <Joey Del Guercio>
 <ProgramName>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <learning basic string uses>
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 
 
 def name_reverse():
-    pass
+    name =  input('enter your name (first last):')
+    first, last = name.split(' ')
+    reversed_name = "{}, {}".format(last, first)
+    print(reversed_name)
+
 
 
 def company_name():
-    pass
+    domain = input('enter a domain:')
+    _, company, _ = domain.split('.')
+    print(company)
 
 
 def initials():
-    pass
+    n_students = eval(input('how many students are in your class?'))
+    for students in range(1, n_students+1):
+        q_string = "what is the name of student {}?".format(students)
+        student_name = (input(q_string))
+        first, last = student_name.split(' ')
+        first_initial, last_initial = first[0], last[0]
+        student_initials = "{}{}".format(first_initial,last_initial)
+        print(student_initials)
 
 
 def names():
-    pass
+    names_input = input('enter a comma-separated list of names:')
+    names_list = names_input.split(',')
+    print(names_list)
+    for name in names_list:
+        first, last = name.split(' ')
+        first_initial, last_initial = first[0], last[0]
+        name_initials = "{}{}".format(first_initial, last_initial)
+        print(name_initials)
+
+
 
 
 def thirds():
---
+++ labs/lab4/lab4.py
@@ -0,0 +1,45 @@
+from graphics import *
+import time
+
+
+def greetingcard():
+    width = 500
+    height = 400
+    win = GraphWin("Clicks", width, height)
+
+    background = Rectangle(Point(0,0), Point(500,400))
+    background.setFill("Pink")
+    background.setOutline("Pink")
+    background.draw(win)
+
+    heart = Polygon(Point(250,150), Point(275,125), Point(320,150), Point(290, 200), Point(265, 250), Point(250, 275), Point(235, 250), Point(210, 200), Point(180, 150), Point(225, 125))
+    heart.setFill("Red")
+    heart.setOutline("Black")
+    heart.draw(win)
+
+    arrowbody = Rectangle(Point(30,200), Point(95,198))
+    arrowbody.setFill("Black")
+    arrowbody.setOutline("Black")
+    arrowbody.draw(win)
+
+    arrowhead = Polygon(Point(95, 202), Point(95, 196), Point(102, 199))
+    arrowhead.setFill("Black")
+    arrowhead.setOutline("Black")
+    arrowhead.draw(win)
+
+    greetingloc = Point(250, 100)
+    greeting = Text(greetingloc, "Happy Valentines Day!")
+    greeting.draw(win)
+
+    for i in range(37):
+        arrowbody.move(3,0)
+        arrowhead.move(3,0)
+        time.sleep(.04)
+
+    inst_pt = Point(250, 380)
+    instructions = Text(inst_pt, "Click to close.")
+    instructions.draw(win)
+    win.getMouse()
+    win.close()
+
+    input()
