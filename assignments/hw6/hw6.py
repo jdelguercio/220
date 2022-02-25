6a36dba774028fe12482901c940bdb3eae5b51b4
jdelguercio <delguerciojj@g.cofc.edu>
2022-02-24 20:13:06 -0500

    hw6 done, kinda

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -10,33 +10,90 @@ I certify that this assignment is entirely my own work.
 I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
+from math import pi
 
 def cash_converter():
-    pass
+    cash_int = float(input("enter an integer:"))
+    cash_float = "{:.2f}".format(cash_int)
+    print("That is ${}".format(cash_float))
 
 
 def encode():
-    pass
-
+    message, key = input('enter a message:'), eval(input('enter a key:'))
+    uni = []
+    uniconv = []
+    convback = []
+    for i in message:
+        conv1 = ord(i)
+        uni.append(conv1)
+    for j in uni:
+        conv2 = j + key
+        uniconv.append(conv2)
+    for q in uniconv:
+        conv3 = chr(q)
+        convback.append(conv3)
+    print(''.join(convback))
 
 def sphere_area(radius):
-    pass
-
+    surf_area = 4.0 * pi * radius**2.0
+    return surf_area
 
 def sphere_volume(radius):
-    pass
+    vol = (4.0/3.0) * pi * radius**3.0
+    return vol
 
 
 def sum_n(number):
-    pass
+    sum = 0
+    for i in range(1,number+1):
+        sum += i
+    return sum
 
 
 def sum_n_cubes(number):
-    pass
+    sum = 0
+    for i in range(1, number + 1):
+        sum += (i**3)
+    return sum
 
 
 def encode_better():
-    pass
+    message, key = input('enter a message:'), input('enter a key:')
+    key_lengthened = (key * (len(message)//len(key)+1))[:len(message)]
+    key_list, message_list = list(key_lengthened), list(message)
+    key_list_uni = []
+    message_list_uni = []
+    output_ord = []
+    output_char = []
+
+    for i in key_list:
+        uni_keyk = ord(i)
+        key_list_uni.append(uni_keyk)
+    print(key_list_uni)
+    for j in message_list:
+        uni_keym = ord(j)
+        message_list_uni.append(uni_keym)
+    print(message_list_uni)
+    for q in range(0, len(message)):
+        key_combine = (((key_list_uni[q] - 65) + (message_list_uni[q] - 65)) % 57) + 64
+        output_ord.append(key_combine)
+    print(output_ord)
+    for z in output_ord:
+        out_char = chr(z)
+        output_char.append(out_char)
+    print(output_char)
+    out_string = ''.join(output_char)
+    print(out_string)
+
+
+
+
+
+
+    uni_message = []
+    conv_message = []
+
+
 
 
 if __name__ == '__main__':
