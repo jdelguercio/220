3bc4671e68307daa0f0d4ba230c62a4c7fc0750d
jdelguercio <delguerciojj@g.cofc.edu>
2022-03-05 09:58:03 -0500

    hw7 done

--- assignments/hw7/hw7.py
+++ assignments/hw7/hw7.py
@@ -1,34 +1,62 @@
-"""
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
-"""
 
 
-def number_words(in_file_name, out_file_name):
-    pass
-
+def number_words(in_file_name, out_file_name): # done
+    num = 0
+    in_file = open(in_file_name, 'r')
+    out_file = open(out_file_name, 'w')
+    for line in in_file:
+        for word in line.split():
+            num += 1
+            print('{} {}'.format(num, word), sep='\n', file=out_file)
 
 def hourly_wages(in_file_name, out_file_name):
-    pass
+    in_file = open(in_file_name, 'r')
+    out_file = open(out_file_name, 'w')
+    for line in in_file:
+        first, last, hourly, hours = ' '.split(line)
+        hourly = hourly + 1.65
+        out_list = [first, last, hourly, hours]
+        for word in out_list:
+            print(word, file=out_file)
+            print(sep='/n', file=out_file)
 
 
-def calc_check_sum(isbn):
-    pass
+def calc_check_sum(isbn): # done
+    isbn = isbn.replace('-', '')
+    position = 11
+    sum = 0
+    for char in isbn:
+        position += -1
+        sum += eval(char) * position
+    return sum
+
+
+
 
 
 def send_message(file_name, friend_name):
-    pass
+    in_file = open(file_name, 'r')
+    out_file_name = '{}.txt'.format(friend_name)
+    out_file = open(out_file_name, 'x')
+    for line in in_file:
+        print(line, sep='\n', file=out_file)
 
 
 def encode():
-    pass
+    message, key = input('enter a message:'), eval(input('enter a key:'))
+    uni = []
+    uniconv = []
+    convback = []
+    for i in message:
+        conv1 = ord(i)
+        uni.append(conv1)
+    for j in uni:
+        conv2 = j + key
+        uniconv.append(conv2)
+    for q in uniconv:
+        conv3 = chr(q)
+        convback.append(conv3)
+    print(''.join(convback))
 
 
 def send_safe_message(file_name, friend_name, key):
